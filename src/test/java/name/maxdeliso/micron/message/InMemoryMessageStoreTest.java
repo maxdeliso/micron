a39f7eb967a9792c4a7f1d36864402f591e04b46
Max DeLiso <me@maxdeliso.name>
2019-04-29 18:35:13 -0400

    add unit tests

--- src/test/java/name/maxdeliso/micron/message/InMemoryMessageStoreTest.java
+++ src/test/java/name/maxdeliso/micron/message/InMemoryMessageStoreTest.java
@@ -1,5 +1,41 @@
 package name.maxdeliso.micron.message;
 
+import name.maxdeliso.micron.peer.PeerRegistry;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
 public class InMemoryMessageStoreTest {
-  // TODO: add unit tests
+
+  private static final int TEST_MESSAGE_COUNT = 8;
+
+  private static final String TEST_MESSAGE = "test-message";
+
+  @Mock
+  private PeerRegistry peerRegistry;
+
+  private MessageStore messageStore;
+
+  @Before
+  public void buildMessageStore() {
+    messageStore = new InMemoryMessageStore(TEST_MESSAGE_COUNT, peerRegistry);
+  }
+
+  @Test
+  public void testPutGet() {
+    messageStore.add(TEST_MESSAGE);
+
+    final Optional<String> messageOpt = messageStore.get(0);
+
+    assertTrue(messageOpt.isPresent());
+    assertEquals(messageOpt.get(), TEST_MESSAGE);
+  }
 }
