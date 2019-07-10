a8af5a74f1a9d709bee6783d3151a99749161ccb
Max DeLiso <me@maxdeliso.name>
2019-07-09 20:44:48 -0400

    run spotbugs and checkstyle during the build, add javadoc comments

--- src/main/java/name/maxdeliso/micron/looper/toggle/DelayedToggler.java
+++ src/main/java/name/maxdeliso/micron/looper/toggle/DelayedToggler.java
@@ -1,11 +1,8 @@
 package name.maxdeliso.micron.looper.toggle;
 
 import lombok.extern.slf4j.Slf4j;
-import name.maxdeliso.micron.looper.EventLooper;
 
-import java.io.IOException;
 import java.util.concurrent.DelayQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 public class DelayedToggler implements Runnable {
