ff2fbead0d3f216b01d84391fa26752170c39dc8
Max DeLiso <me@maxdeliso.name>
2019-09-09 22:02:03 -0400

    use a class for an array of atomic integers

--- src/main/java/name/maxdeliso/micron/slots/InMemorySlotManager.java
+++ src/main/java/name/maxdeliso/micron/slots/InMemorySlotManager.java
@@ -1,48 +1,43 @@
 package name.maxdeliso.micron.slots;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class InMemorySlotManager implements SlotManager {
-  private final List<AtomicInteger> occupationCounts;
+  private final AtomicIntegerArray occupationCounts;
 
   public InMemorySlotManager(int size) {
-    occupationCounts = new ArrayList<>(size);
-
-    for (int i = 0; i < size; i++) {
-      occupationCounts.add(new AtomicInteger(0));
-    }
+    occupationCounts = new AtomicIntegerArray(size);
   }
 
   @Override
   public boolean positionOccupied(int pos) {
-    return occupationCounts.get(pos).get() > 0;
+    return occupationCounts.get(pos) > 0;
   }
 
   @Override
   public void incrementOccupants(int pos) {
-    occupationCounts.get(pos).incrementAndGet();
+    occupationCounts.incrementAndGet(pos);
   }
 
   @Override
   public void decrementOccupants(int pos) {
-    occupationCounts.get(pos).decrementAndGet();
+    occupationCounts.decrementAndGet(pos);
   }
 
   @Override
   public int nextNotSet(int pos) {
-    final int initialPosition = pos;
-    int i = (initialPosition + 1) % occupationCounts.size();
-    while (i != initialPosition && occupationCounts.get(i).get() == 0) {
-      i = (i + 1) % occupationCounts.size();
-    }
+    int i = pos;
+
+    do {
+      i = (i + 1) % size();
+    } while (i != pos && occupationCounts.get(i) == 0);
+
     return i;
   }
 
   @Override
   public int size() {
-    return occupationCounts.size();
+    return occupationCounts.length();
   }
 
   @Override
@@ -50,15 +45,15 @@ public class InMemorySlotManager implements SlotManager {
     final StringBuilder sb = new StringBuilder();
     int matches = 0;
 
-    for (int i = 0; i < occupationCounts.size(); i++) {
-      final AtomicInteger occupationCount = occupationCounts.get(i);
+    for (int i = 0; i < occupationCounts.length(); i++) {
+      final int count = occupationCounts.get(i);
 
-      if (occupationCount.get() > 0) {
+      if (count > 0) {
         if (matches > 0) {
           sb.append(", ");
         }
 
-        sb.append(occupationCount.get());
+        sb.append(count);
         sb.append(" @ ");
         sb.append(i);
         matches++;
